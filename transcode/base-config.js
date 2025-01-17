f0cc261735adb728b15e0f19a4fcf9385d8c185d
TC McCarthy <tc.mccarthy1@gmail.com>
2025-01-17 11:28:24 -0500

    Adds concurrent transcode support

--- transcode/base-config.js
+++ transcode/base-config.js
@@ -7,7 +7,7 @@ export function aspect_round(val) {
 const config = {
   encode_version: "20250108a",
   concurrent_file_checks: 50,
-  concurrent_transcodes: 5,
+  concurrent_transcodes: 8,
   profiles: [
     {
       name: "uhd",
