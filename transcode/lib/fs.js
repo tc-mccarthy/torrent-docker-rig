02d8d60c38cdb698abe33acba72299cb742ceb89
TC McCarthy <tc.mccarthy@newsday.com>
2025-05-15 15:00:18 -0400

    More revert of escpe_file_path

--- transcode/lib/fs.js
+++ transcode/lib/fs.js
@@ -5,7 +5,7 @@ import exec_promise from './exec_promise';
 import config from './config';
 
 export function escape_file_path (file) {
-  return file.replace(/(['])/g, "'\\''").replace(/\n+$/, '');
+  return file.replace(/(['])/g, "\x27").replace(/\n+$/, '');
 }
 
 export async function trash (file) {
