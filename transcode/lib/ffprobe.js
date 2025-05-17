eb6346dfc8b240db022b6ccdaa7e1788dbc70bc9
TC McCarthy <tc.mccarthy@newsday.com>
2025-05-17 19:14:17 -0400

    Adopts fluent-ffmpeg's ffprobe implementation

--- transcode/lib/ffprobe.js
+++ transcode/lib/ffprobe.js
@@ -1,14 +1,17 @@
-import * as fs from 'fs';
-import { aspect_round } from './base-config';
-import { trash } from './fs';
-import logger from './logger';
-import {ffprobe} from 'fluent-ffmpeg'
+import * as fs from "fs";
+import { aspect_round } from "./base-config";
+import { trash } from "./fs";
+import logger from "./logger";
+import { ffprobe } from "fluent-ffmpeg";
 
-export function ffprobe_promise(file){
+export function ffprobe_promise(file) {
   return new Promise((resolve, reject) => {
+    logger.info(file, {
+      label: "Probing file using ffprobe method from fluent-ffmpeg wrapper",
+    });
     ffprobe(file, (err, data) => {
       if (err) {
-        logger.error('FFPROBE FAILED', err);
+        logger.error("FFPROBE FAILED", err);
         return reject(err);
       }
       resolve(data);
@@ -16,24 +19,26 @@ export function ffprobe_promise(file){
   });
 }
 
-export default async function ffprobe_func (file) {
+export default async function ffprobe_func(file) {
   try {
-    // confirm the file exists 
-    if(!fs.existsSync(file)){
+    // confirm the file exists
+    if (!fs.existsSync(file)) {
       return new Error(`File does not exist: ${file}`);
     }
 
     const data = await ffprobe_promise(file);
+
+    logger.info(data, { label: "FFProbe complete" });
 
     data.format.duration = +data.format.duration;
     data.format.size = +data.format.size;
     data.format.bit_rate = +data.format.bit_rate;
     data.format.size = +data.format.size / 1024;
 
-    const video = data.streams.find((s) => s.codec_type === 'video');
+    const video = data.streams.find((s) => s.codec_type === "video");
 
     if (video.display_aspect_ratio) {
-      const [width, height] = video.display_aspect_ratio.split(':');
+      const [width, height] = video.display_aspect_ratio.split(":");
       video.aspect = aspect_round(width / height);
     } else {
       video.aspect = aspect_round(video.width / video.height);
@@ -44,7 +49,7 @@ export default async function ffprobe_func (file) {
     if (/command\s+failed/gi.test(e.message)) {
       trash(file);
     }
-    logger.error('FFPROBE FAILED', e);
+    logger.error("FFPROBE FAILED", e);
     return false;
   }
 }
