fb3da97009789ca97a9ed8f1c52c85f7c9488e47
TC McCarthy <tc.mccarthy@newsday.com>
2025-05-18 18:25:48 -0400

    Random wait before starting next transcode

--- transcode/lib/transcode_loop.js
+++ transcode/lib/transcode_loop.js
@@ -23,20 +23,14 @@ export default async function transcode_loop (idx = 0) {
     const file = filelist[idx];
     await transcode(file);
     logger.info('TRANSCODE COMPLETE');
-
-    // if there are more files, run the loop again
-    if (filelist.length > 1) {
-      logger.info('MORE FILES TO TRANSCODE. RUNNING LOOP AGAIN');
-
-      // generate a random number between 0 and 2 seconds
-      const randomDelay = Math.floor(Math.random() * 2000);
-
-      await wait(randomDelay);
-      return transcode_loop();
-    }
   } catch (e) {
     logger.error('TRANSCODE LOOP ERROR. RESTARTING LOOP');
     console.error(e);
+  } finally {
+    // generate a random number between 0 and 2 seconds
+    const randomDelay = Math.floor(Math.random() * 2000);
+
+    await wait(randomDelay);
     return transcode_loop();
   }
 }
