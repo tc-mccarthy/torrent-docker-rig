cce786b354534701fe4a62c4ba8e8737eff78643
TC McCarthy <tc.mccarthy@newsday.com>
2025-05-18 18:29:51 -0400

    Random wait before starting next transcode

--- transcode/lib/transcode_loop.js
+++ transcode/lib/transcode_loop.js
@@ -1,12 +1,12 @@
-import logger from './logger';
-import generate_filelist from './generate_filelist';
-import update_status from './update_status';
-import transcode from './transcode';
-import wait from './wait';
+import logger from "./logger";
+import generate_filelist from "./generate_filelist";
+import update_status from "./update_status";
+import transcode from "./transcode";
+import wait, { getRandomDelay } from "./wait";
 
-export default async function transcode_loop (idx = 0) {
+export default async function transcode_loop(idx = 0) {
   try {
-    logger.info('STARTING TRANSCODE LOOP');
+    logger.info("STARTING TRANSCODE LOOP");
     const filelist = await generate_filelist();
     logger.info(
       `PRIMARY FILE LIST ACQUIRED. THERE ARE ${filelist.length} FILES TO TRANSCODE.`
@@ -18,17 +18,17 @@ export default async function transcode_loop (idx = 0) {
     }
 
     await update_status();
-    logger.info('BEGINNING TRANSCODE');
+    logger.info("BEGINNING TRANSCODE");
 
     const file = filelist[idx];
     await transcode(file);
-    logger.info('TRANSCODE COMPLETE');
+    logger.info("TRANSCODE COMPLETE");
   } catch (e) {
-    logger.error('TRANSCODE LOOP ERROR. RESTARTING LOOP');
+    logger.error("TRANSCODE LOOP ERROR. RESTARTING LOOP");
     console.error(e);
   } finally {
     // generate a random number between 0 and 2 seconds
-    const randomDelay = Math.floor(Math.random() * 2000);
+    const randomDelay = getRandomDelay(0, 2);
 
     await wait(randomDelay);
     return transcode_loop();
--- transcode/lib/wait.js
+++ transcode/lib/wait.js
@@ -6,3 +6,9 @@ export default function wait(sec) {
     setTimeout(resolve, sec * 1000);
   });
 }
+
+export function getRandomDelay(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
