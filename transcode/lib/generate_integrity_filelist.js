113ab52c5d54cb6d5627b32cc000a93071e8a1c5
TC McCarthy <tc.mccarthy@newsday.com>
2025-05-24 13:42:57 -0400

    More work to improve lock stripping logic on generate_integrity_filelist

--- transcode/lib/generate_integrity_filelist.js
+++ transcode/lib/generate_integrity_filelist.js
@@ -47,6 +47,10 @@ export default async function generate_integrity_filelist() {
   });
   filelist = filelist.filter((f) => !f.locked);
 
+  logger.info(filelist, {
+    label: "INTEGRITY CHECK FILELIST",
+  });
+
   // send back full list
   return filelist.map((f) => f.path);
 }
