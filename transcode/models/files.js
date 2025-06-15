f98b6c19873a22b8e314a504580b7009ae3b93ee
TC McCarthy <tc.mccarthy@newsday.com>
2025-06-15 10:49:26 -0400

    Resolves race condition in setLock

--- transcode/models/files.js
+++ transcode/models/files.js
@@ -101,8 +101,8 @@ schema.methods.setLock = async function (type, sec = 10) {
   }
   const lock = await memcached.set(`${type}_lock_${this._id}`, 'locked', sec);
 
-  this.lock[type] = dayjs().add(10, 'seconds').toDate();
-  await this.save();
+  File.update({_id: this._id}, { $set: { [`lock.${type}`]: dayjs().add(sec, 'seconds').toDate() } });
+  
   return lock;
 };
 
