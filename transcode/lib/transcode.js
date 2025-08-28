d7a48a60749be031207e5b96b3c39f969bf05baa
TC McCarthy <tc.mccarthy@newsday.com>
2025-08-28 19:02:46 -0400

    Removes quotes from audio filter

--- transcode/lib/transcode.js
+++ transcode/lib/transcode.js
@@ -246,7 +246,7 @@ export default function transcode (file) {
             `-c:a:${idx} ${audio.codec}`,
             audio.bitrate && `-b:a:${idx} ${audio.bitrate}`,
             audio.channels && `-ac:${idx} ${audio.channels}`,
-            audio.filter && `-filter:a:${idx} '${audio.filter}'`,
+            audio.filter && `-filter:a:${idx} ${audio.filter}`,
             `-map_metadata:s:a:${idx} 0:s:a:${idx}`
           ].filter(Boolean)))
         .outputOptions(transcode_instructions.subtitles
